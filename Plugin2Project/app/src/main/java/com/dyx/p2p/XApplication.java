428271e4ffbf9d41416835a0348f3427fad2ffc9
doubleda <dayongxin@aliyun.com>
2017-07-03 22:25:23 +0800

    提交插件1&2

---
+++ Plugin2Project/app/src/main/java/com/dyx/p2p/XApplication.java
@@ -0,0 +1,19 @@
+package com.dyx.p2p;
+
+import com.qihoo360.replugin.RePluginApplication;
+
+/**
+ * project name：Plugin2Project
+ * class describe：
+ * create person：dayongxin
+ * create time：2017/7/3 下午10:24
+ * alter person：dayongxin
+ * alter time：2017/7/3 下午10:24
+ * alter remark：
+ */
+public class XApplication extends RePluginApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
